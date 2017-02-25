d3c983864ea76099a8be3bc5e46f0ec479224789
Sebastian Fröstl <sebastian@froestl.com>
2017-02-25 19:17:39 +0100

    feat(bot) use ai

--- src/BotController.ts
+++ src/BotController.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
-import { getEvents, sendTextMessage } from './helpers';
+import { getEvents, sendTextMessage, indicateWriting } from './FbApi';
+import ai from './ai';
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const router: Router = Router();
 
@@ -21,11 +22,17 @@ router.post('/webhook', (req: Request, res: Response) => {
     console.log('webhook triggered! events=', events.length);
 	for (let i = 0; i < events.length; i++) {
 		const event = events[i];
+		const senderId = event.sender.id;
+		indicateWriting(senderId);
         console.log('---EVENT=', event);
 		if (event.message && event.message.text) {
-			const senderId = event.sender.id;
 			const text = event.message.text;
-			sendTextMessage(senderId, `Echo: ${text.substring(0, 200)}`);
+			ai
+				.ask(text)
+				.then((aiResponse) => {
+					console.log('aiResponse=', aiResponse);
+					sendTextMessage(senderId, `Echo: ${text.substring(0, 200)}`)
+				});
 		}
 	}
 	res.sendStatus(200);
