a1aa68fb516143f812c44066b98af1d528e221c8
Sebastian Fröstl <sebastian@froestl.com>
2017-02-25 21:07:22 +0100

    chore(*) add web pack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const { CheckerPlugin } = require('awesome-typescript-loader')
+const path = require('path');
+
+module.exports = {
+
+    entry: './index.ts',
+
+    target: 'node',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx', '.jsx']
+    },
+
+    devtool: 'source-map',
+
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                exclude: [/(node_modules)$/],
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        new CheckerPlugin()
+    ]
+};
