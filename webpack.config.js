503c614d761b1e3b982c4c080439784283ad0c71
Sebastian Fröstl <sebastian@froestl.com>
2017-02-26 12:58:22 +0100

    fix(webpack) clean file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
-const { CheckerPlugin } = require('awesome-typescript-loader')
+const { CheckerPlugin } = require('awesome-typescript-loader');
 const path = require('path');
 
 module.exports = {
-
     entry: './index.ts',
-
     target: 'node',
-
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist'),
     },
-
     resolve: {
         extensions: ['.ts', '.js', '.tsx', '.jsx']
     },
-
     devtool: 'source-map',
-
     module: {
         loaders: [
             {
@@ -27,7 +21,6 @@ module.exports = {
             }
         ]
     },
-
     plugins: [
         new CheckerPlugin()
     ]
