92c148e5db7ebd9c0fce31b3876c1371e6e156a0
Sebastian Fröstl <sebastian@froestl.com>
2017-02-26 16:30:06 +0100

    fix(*) tab indentations

--- src/BotController.ts
+++ src/BotController.ts
@@ -7,82 +7,82 @@ const router: Router = Router();
 export const getEvents = (req: Request) => req.body.entry[0].messaging;
 
 function handleWebhook(req: Request, res: Response) {
-	console.log('received webhook body=', req.body);
+    console.log('received webhook body=', req.body);
 
-	getEvents(req).forEach((messagingEvent) => {
-		if (messagingEvent.message) {
-			receivedMessage(messagingEvent);
-		} else if (messagingEvent.postback) {
-			receivedPostback(messagingEvent);
-		} else {
-			console.log('Webhook received unknown messagingEvent: ', messagingEvent);
-		}
-	});
-	res.sendStatus(200);
+    getEvents(req).forEach((messagingEvent) => {
+        if (messagingEvent.message) {
+            receivedMessage(messagingEvent);
+        } else if (messagingEvent.postback) {
+            receivedPostback(messagingEvent);
+        } else {
+            console.log('Webhook received unknown messagingEvent: ', messagingEvent);
+        }
+    });
+    res.sendStatus(200);
 }
 
 function receivedMessage(event) {
-	const text: string = event.message.text;
-	const senderId: string = event.sender.id;
+    const text: string = event.message.text;
+    const senderId: string = event.sender.id;
 
-	indicateWriting(senderId);
+    indicateWriting(senderId);
 
-	ai
-		.ask(senderId, text)
-		.then((aiResponse: any) => {
-			console.log('---aiResponse=', aiResponse);
-			const { speech } = aiResponse.result.fulfillment;
-			sendTextMessage(senderId, speech)
-				.catch(console.log);
-		})
-		.catch((error) => {
-			console.log('API Error', error);
-		});
+    ai
+        .ask(senderId, text)
+        .then((aiResponse: any) => {
+            console.log('---aiResponse=', aiResponse);
+            const { speech } = aiResponse.result.fulfillment;
+            sendTextMessage(senderId, speech)
+                .catch(console.log);
+        })
+        .catch((error) => {
+            console.log('API Error', error);
+        });
 }
 
 function receivedPostback(event) {
-	const payload: string = event.postback.payload;
-	const senderId: string = event.sender.id;
+    const payload: string = event.postback.payload;
+    const senderId: string = event.sender.id;
 
-	indicateWriting(senderId);
+    indicateWriting(senderId);
 
-	if (payload === 'GREAT') {
-		sendTextMessage(senderId, `I'm glad you feel great!`);
-	}
+    if (payload === 'GREAT') {
+        sendTextMessage(senderId, `I'm glad you feel great!`);
+    }
 
-	if (payload === 'BAD') {
-		sendTextMessage(senderId, `I'm sorry to hear you feel bad!`);
-	}
+    if (payload === 'BAD') {
+        sendTextMessage(senderId, `I'm sorry to hear you feel bad!`);
+    }
 }
 
 function sendMood(id: string) {
-	console.log('Sending mood query!');
-	sendButton(id, 'How do you feel right now?');
+    console.log('Sending mood query!');
+    sendButton(id, 'How do you feel right now?');
 }
 
 router.get('/', (req: Request, res: Response) => {
-	res.send('Hello world I am Stella.')
+    res.send('Hello world I am Stella.')
 });
 
 router.get('/webhook', (req: Request, res: Response) => {
-	console.log('webhook validation!');
-	if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
-		res.send(req.query['hub.challenge']);
-	} else {
-		res.send('Error, wrong token');
-	}
+    console.log('webhook validation!');
+    if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Error, wrong token');
+    }
 });
 
 router.get('/mood', (req, res) => {
-	const userId = req.query['user'];
-	sendMood(userId);
-	res.sendStatus(200);
+    const userId = req.query['user'];
+    sendMood(userId);
+    res.sendStatus(200);
 });
 
 router.post('/message', (req, res) => {
-	const { userId, message } = req.body;
-	sendTextMessage(userId, message);
-	res.sendStatus(200);
+    const { userId, message } = req.body;
+    sendTextMessage(userId, message);
+    res.sendStatus(200);
 });
 
 router.post('/webhook', handleWebhook);
--- src/FbApi.ts
+++ src/FbApi.ts
@@ -7,120 +7,120 @@ const FB_ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN;
 const USER_FIELDS = 'first_name,last_name,profile_pic,locale,timezone,gender';
 
 interface IFbMessageUser {
-	id: string;
+    id: string;
 };
 
 interface IFbResponse {
-	recipient: IFbMessageUser;
-	message?: any;
-	sender_action?: any,
+    recipient: IFbMessageUser;
+    message?: any;
+    sender_action?: any,
 };
 
 export interface IFacebookProfile {
-	first_name?: string;
-	last_name?: string;
-	profile_pic?: string;
-	locale?: string;
-	timezone?: number;
-	gender?: string;
+    first_name?: string;
+    last_name?: string;
+    profile_pic?: string;
+    locale?: string;
+    timezone?: number;
+    gender?: string;
 };
 
 function dispatchMessage(response: IFbResponse) {
-	const { recipient, message } = response;
-	console.log(`dispatch message to user id=${recipient.id}`);
-	const config = {
-		url: FB_MSG_URL,
-		qs: { access_token: FB_ACCESS_TOKEN },
-		method: 'POST',
-		json: {
-			recipient,
-			message,
-		}
-	};
-	return new Promise((resolve: Function, reject: Function) => {
-		request(
-			config,
-			function (error, response, body) {
-				if (error) {
-					reject(error);
-				} else if (response.body.error) {
-					reject(response.body.error);
-				} else {
-					resolve({ response, body });
-				}
-			});
-	});
+    const { recipient, message } = response;
+    console.log(`dispatch message to user id=${recipient.id}`);
+    const config = {
+        url: FB_MSG_URL,
+        qs: { access_token: FB_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient,
+            message,
+        }
+    };
+    return new Promise((resolve: Function, reject: Function) => {
+        request(
+            config,
+            function (error, response, body) {
+                if (error) {
+                    reject(error);
+                } else if (response.body.error) {
+                    reject(response.body.error);
+                } else {
+                    resolve({ response, body });
+                }
+            });
+    });
 }
 
 export const indicateWriting = (id: string) => {
-	const response: IFbResponse = {
-		recipient: { id },
-		sender_action: 'typing_on',
-	};
-	return dispatchMessage(response);
+    const response: IFbResponse = {
+        recipient: { id },
+        sender_action: 'typing_on',
+    };
+    return dispatchMessage(response);
 };
 
 export const sendTextMessage = (id: string, text: string) => {
-	console.log(`sening message to id=${id} message=${text}`);
-	const response: IFbResponse = {
-		recipient: { id },
-		message: { text },
-	};
-	return dispatchMessage(response);
+    console.log(`sening message to id=${id} message=${text}`);
+    const response: IFbResponse = {
+        recipient: { id },
+        message: { text },
+    };
+    return dispatchMessage(response);
 };
 
 export const sendButton = (id: string, text: string) => {
-	console.log(`sening button to id=${id} message=${text}`);
-	const response: IFbResponse = {
-		recipient: { id },
-		message: {
-			attachment: {
-				type: 'template',
-				payload: {
-					template_type: 'button',
-					text,
-					buttons: [{
-						type: 'postback',
-						title: 'Great',
-						payload: 'GREAT',
-					},
-					{
-						type: 'postback',
-						title: 'Bad',
-						payload: 'BAD',
-					}],
-				},
-			},
-		},
-	};
-	return dispatchMessage(response);
+    console.log(`sening button to id=${id} message=${text}`);
+    const response: IFbResponse = {
+        recipient: { id },
+        message: {
+            attachment: {
+                type: 'template',
+                payload: {
+                    template_type: 'button',
+                    text,
+                    buttons: [{
+                        type: 'postback',
+                        title: 'Great',
+                        payload: 'GREAT',
+                    },
+                    {
+                        type: 'postback',
+                        title: 'Bad',
+                        payload: 'BAD',
+                    }],
+                },
+            },
+        },
+    };
+    return dispatchMessage(response);
 };
 
 export const getUserDetails = (id: string): Promise<IFacebookProfile> => {
-	const config = {
-		url: `${BASE_API}/${id}`,
-		qs: { fields: USER_FIELDS, access_token: FB_ACCESS_TOKEN },
-		method: 'GET',
-	};
-	return new Promise((resolve: Function, reject: Function) => {
-		request(
-			config,
-			(err, res, body) => {
-				if (err) {
-					console.log('facebook: could not get user from facebook err=', err);
-					reject(err);
-				}
-				if (res.statusCode >= 400) {
-					console.log('facebook: could not get user from facebook err=', body);
-					reject(new Error(`facebook: Error getting profile for user ${JSON.stringify(body)} ${JSON.stringify(id)}`));
-				}
-				var obj = {};
-				try {
-					obj = JSON.parse(body);
-				} catch (e) {
-					obj = body;
-				}
-				return resolve(<any>obj);
-			});
-	});
+    const config = {
+        url: `${BASE_API}/${id}`,
+        qs: { fields: USER_FIELDS, access_token: FB_ACCESS_TOKEN },
+        method: 'GET',
+    };
+    return new Promise((resolve: Function, reject: Function) => {
+        request(
+            config,
+            (err, res, body) => {
+                if (err) {
+                    console.log('facebook: could not get user from facebook err=', err);
+                    reject(err);
+                }
+                if (res.statusCode >= 400) {
+                    console.log('facebook: could not get user from facebook err=', body);
+                    reject(new Error(`facebook: Error getting profile for user ${JSON.stringify(body)} ${JSON.stringify(id)}`));
+                }
+                var obj = {};
+                try {
+                    obj = JSON.parse(body);
+                } catch (e) {
+                    obj = body;
+                }
+                return resolve(<any>obj);
+            });
+    });
 };
